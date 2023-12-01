4d2496fcb94c80e65dc02dd562bacb5891e1bc6f
Monica Murray <Monica.rickerby@gmail.com>
2023-12-01 14:05:34 +0000

    create loop to iterate through input strings to extract digits

--- day_1.py
+++ day_1.py
@@ -3,7 +3,7 @@ import re
 input_str = "nqninenmvnpsz874 8twofpmpxkvvdnpdnlpkhseven4ncgkb six8shdkdcdgseven8xczqrnnmthreecckfive"
 
 strings_by_line = input_str.split(' ')
-print(strings_by_line)
+print(input_str.split())
 
 # using re.search() not perfect!
 # first_digit = re.search(r'\d', input_str)
@@ -15,18 +15,33 @@ print(strings_by_line)
 
 
 # using re.findall() works beautifully
-str_digits = re.findall(r'\d+', strings_by_line[0])[0]
-print(str_digits)
+# str_digits = re.findall(r'\d+', input_str)[0]
+# print(str_digits)
 
-if str_digits:
-    first_digit = str_digits[0]
-    print(first_digit)
-    last_digit = str_digits[-1]
-    print(last_digit)
+# if str_digits:
+#     first_digit = str_digits[0]
+#     print(first_digit)
+#     last_digit = str_digits[-1]
+#     print(last_digit)
     
-input_str_calibration_value = [first_digit+last_digit,]
+# input_str_calibration_value = [first_digit+last_digit,]
 
-print(input_str_calibration_value)
+# print(input_str_calibration_value)
 
+# create a loop to step through strings_by_line's list of strings, adding the first and last digits from each string to the input_str_calibration_value list with each iteration of the loop
+
+for i in input_str:
+    str_digits = re.findall(r'\d', i)
+    print(str_digits)
+
+#     if str_digits:
+#         first_digit = str_digits[0]
+#         print(first_digit)
+#         last_digit = str_digits[-1]
+#         print(last_digit)
+    
+#     input_str_calibration_value = [first_digit+last_digit,]
+
+# print(input_str_calibration_value)
 
 
