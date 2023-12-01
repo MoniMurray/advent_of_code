70a9b80e3cf0f2121a48d21f765d78fcd9feae76
Monica Murray <Monica.rickerby@gmail.com>
2023-12-01 15:50:45 +0000

    define an array outside of for loop, use append function to add each iteration value to the array

--- day_1.py
+++ day_1.py
@@ -20,33 +20,33 @@ input_str.close()
 
 
 # using re.findall() works beautifully
-str_digits = re.findall(r'\d+', strings_by_line)[-1]
-print(str_digits)
+# str_digits = re.findall(r'\d+', strings_by_line)[-1]
+# print(str_digits)
 
-if str_digits:
-    first_digit = str_digits[0]
-    print(first_digit)
-    last_digit = str_digits[-1]
-    print(last_digit)
+# if str_digits:
+#     first_digit = str_digits[0]
+#     print(first_digit)
+#     last_digit = str_digits[-1]
+#     print(last_digit)
     
-input_str_calibration_value = [first_digit+last_digit,]
+# input_str_calibration_value = [first_digit+last_digit,]
 
-print(input_str_calibration_value)
+# print(input_str_calibration_value)
 
 # create a loop to step through strings_by_line's list of strings, adding the first and last digits from each string to the input_str_calibration_value list with each iteration of the loop
 
-# for i in input_str:
-#     str_digits = re.findall(r'\d', i)
-#     print(str_digits)
+str_digits = re.findall(r'\d+', strings_by_line)
+input_str_calibration_value = []
 
-#     if str_digits:
-#         first_digit = str_digits[0]
-#         print(first_digit)
-#         last_digit = str_digits[-1]
-#         print(last_digit)
+for d in str_digits:
     
-#     input_str_calibration_value = [first_digit+last_digit,]
+    first_digit = d[0]
+    print(first_digit)
+    last_digit = d[-1]
+    print(last_digit)
+        
+    input_str_calibration_value.append(first_digit+last_digit)
 
-# print(input_str_calibration_value)
+print(input_str_calibration_value)
 
 
