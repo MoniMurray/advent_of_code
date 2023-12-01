6dedf09191578f152e4902127867aa6aec209cf7
Monica Murray <Monica.rickerby@gmail.com>
2023-12-01 16:08:43 +0000

    convert each array entry to int, sum array contents

--- day_1.py
+++ day_1.py
@@ -1,22 +1,10 @@
 import re
 
-# input_str = "nqninenmvnpsz874 8twofpmpxkvvdnpdnlpkhseven4ncgkb six8shdkdcdgseven8xczqrnnmthreecckfive"
-# strings_by_line = input_str.split(' ')
-# print(input_str.split())
-
 # use the splitlines() to read and split a file by lines
 input_str = open("input.yml", "r")
 strings_by_line = input_str.read()
-# print(strings_by_line.splitlines())
+print(strings_by_line.splitlines())
 input_str.close()
-
-# using re.search() not perfect!
-# first_digit = re.search(r'\d', input_str)
-# print(first_digit)
-# last_digit = re.search(r'\d$', input_str)
-# print(last_digit)
-
-# input_str_calibration_value = [first_digit.int(), last_digit.int()]
 
 
 # using re.findall() works beautifully
@@ -33,7 +21,7 @@ input_str.close()
 
 # print(input_str_calibration_value)
 
-# create a loop to step through strings_by_line's list of strings, adding the first and last digits from each string to the input_str_calibration_value list with each iteration of the loop
+# create a loop to step through strings_by_line's list of strings
 
 str_digits = re.findall(r'\d+', strings_by_line)
 input_str_calibration_value = []
@@ -41,12 +29,16 @@ input_str_calibration_value = []
 for d in str_digits:
     
     first_digit = d[0]
-    print(first_digit)
+    # print(first_digit)
     last_digit = d[-1]
-    print(last_digit)
+    # print(last_digit)
         
-    input_str_calibration_value.append(first_digit+last_digit)
+    # add the first and last digits from each string line to the input_str_calibration_value list with each iteration of the loop, converting from str to int
+    input_str_calibration_value.append(int(first_digit+last_digit))
 
-print(input_str_calibration_value)
+# sum the list
+star_1 = sum(input_str_calibration_value)
+print(star_1)
+
 
 
