ba26f4831236273922a0524b541f1b1ce78ecf7f
Monica Murray <Monica.rickerby@gmail.com>
2023-12-01 13:24:06 +0000

    determin how to split string at space to create list of strings to use findall on index point

--- day_1.py
+++ day_1.py
@@ -1,12 +1,21 @@
 import re
 
-input_str = 'nqninenmvnpsz874'
-# using re.search()
+input_str = "nqninenmvnpsz874 8twofpmpxkvvdnpdnlpkhseven4ncgkb six8shdkdcdgseven8xczqrnnmthreecckfive"
+
+strings_by_line = input_str.split(' ')
+print(strings_by_line)
+
+# using re.search() not perfect!
 # first_digit = re.search(r'\d', input_str)
 # print(first_digit)
+# last_digit = re.search(r'\d$', input_str)
+# print(last_digit)
 
-# using re.findall()
-str_digits = re.findall(r'\d+', input_str)[0]
+# input_str_calibration_value = [first_digit.int(), last_digit.int()]
+
+
+# using re.findall() works beautifully
+str_digits = re.findall(r'\d+', strings_by_line[0])[0]
 print(str_digits)
 
 if str_digits:
@@ -14,7 +23,10 @@ if str_digits:
     print(first_digit)
     last_digit = str_digits[-1]
     print(last_digit)
-    input_str_calibration_value = [first_digit+last_digit,]
+    
+input_str_calibration_value = [first_digit+last_digit,]
 
 print(input_str_calibration_value)
+
+
 
